20c2a22687e8febfe9b387fe75ec36d9417009c8
Pararhythm <221648319+pararhythm@users.noreply.github.com>
2026-08-24 10:28:10 +0700

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        let count = {};
+        for (let num of nums) {
+            count[num] = (count[num] || 0) + 1;
+        }
+
+        let buckets = Array.from({ length: nums.length + 1}, () => []);
+        for (let num in count) {
+            let freq = count[num];
+            buckets[freq].push(Number(num))
+        }
+
+        let res = [];
+        for (let i = buckets.length - 1; i > 0; i--) {
+            for (let num of buckets[i]) {      
+                res.push(num);
+                if (res.length === k) return res;
+            }
+        }
+
+    }
+}
