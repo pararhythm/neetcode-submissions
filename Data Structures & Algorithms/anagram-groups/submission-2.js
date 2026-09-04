c02ea45725f545876dcfb216dbd86116172799b7
Pararhythm <221648319+pararhythm@users.noreply.github.com>
2026-09-04 10:09:04 +0700

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let map = new Map();
+        for (let str of strs) {
+            let count = new Array(26).fill(0);
+            for (let char of str) {
+                let index = char.charCodeAt(0) - 'a'.charCodeAt(0);
+                count[index]++;
+            }
+
+            let key = count.join(',');
+            if (!map.has(key)) {
+                map.set(key, []);
+            }
+            map.get(key).push(str);
+        }
+        return Array.from(map.values());
+    }
+}
