510ccce07dc92c67e6e695298e6332b6aca01706
Pararhythm <221648319+pararhythm@users.noreply.github.com>
2026-09-09 10:34:41 +0700

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        let result = [];
+        nums.sort((a,b) => a - b);
+        for (let i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] === nums[i - 1]) {
+                continue;
+            } 
+            
+            let left = i + 1;
+            let right = nums.length - 1;
+            while (left < right) {
+                let sum = nums[i] + nums[right] + nums[left];
+                if (sum < 0) {
+                    left++;
+                }
+                if (sum > 0) {
+                    right--;
+                }
+                if (sum === 0) {
+                    let ans = [nums[i], nums[right], nums[left]];
+                    result.push(ans);
+                    left++;
+                    right--;
+                    while (left < right && nums[left] === nums[left - 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] === nums[right + 1]) {
+                        right--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
